0f686f1c8a4fd1e97a1935effedd8a50945c4873
Geon-qr5 <zzzqr5@gmail.com>
2024-07-29 17:40:47 +0900

    연습

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,22 @@
+import java.util.ArrayList;
+
 public class Book {
 
+    private int number;
+    private String title;
+    private String author;
+    private boolean rent;
+
+    public Book (){
+        
+    }
+
+    public Book (int number, String title, String author, boolean rent){
+        this.number = number;
+        this.title = title;
+        this.author = author;
+        rent = false;
+    }
+
+    ArrayList booklist = new ArrayList<Book>();
 }
