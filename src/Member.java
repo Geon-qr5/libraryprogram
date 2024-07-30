2e23ba1cfc0c32d4ebbda5748c77e7d4939ad440
Geon-qr5 <zzzqr5@gmail.com>
2024-07-31 08:05:40 +0900

    마무리

--- src/Member.java
+++ src/Member.java
@@ -1,3 +1,13 @@
 public class Member {
+    private String name;
+    private boolean isAdmin;
 
+    public Member(){
+        
+    }
+
+    public Member(String name, boolean isAdmin){
+        this.name = name;
+        isAdmin = false;
+    }
 }
